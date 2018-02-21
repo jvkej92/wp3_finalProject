de0fc5168604022236316e90a67936129a39dd7d
Jacob Johnson <loveoutjake@gmail.com>
2018-02-21 16:04:33 -0600

    Turned off debugging mode and slightly modified style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,11 +42,11 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+                            <li><a class="nav-link text-light" href="{{ route('login') }}">Login</a></li>
+                            <li><a class="nav-link text-light" href="{{ route('register') }}">Register</a></li>
                         @else
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
