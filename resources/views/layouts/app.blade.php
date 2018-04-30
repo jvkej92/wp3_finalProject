f09db8e8558807fe09395005c221e1e5345cdd67
Jake Johnson <jake@johnsonjonesgroup.com>
2018-04-30 14:32:09 -0500

    added button to admin nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,6 +54,9 @@
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                @if(Voyager::can('browse_admin'))
+                                        <a class="dropdown-item" href="{{ url('admin') }}">Admin</a>
+                                    @endif
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
